ab33a9128c7c556be1063f52bad3e51d83570752
asmaa-kashef <58347664+asmaa-kashef@users.noreply.github.com>
2021-12-25 15:48:31 +0200

    commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn FaceDetector:app
+web: gunicorn -k eventlet -w 1 app:app --log-file=-
--- camera.py
+++ camera.py
@@ -1,48 +1,48 @@
-# Import OpenCV library
-import cv2
+import threading
+import binascii
+from time import sleep
+from utils import base64_to_pil_image, pil_image_to_base64
 
-# Create Face detection object from OpenCV frontal face Classifier
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade  = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-class VideoCamera(object):
+class Camera(object):
+    def __init__(self, makeup_artist):
+        self.to_process = []
+        self.to_output = []
+        self.makeup_artist = makeup_artist
 
-    def __init__(self):
-        # Using OpenCV initiate front facing built-in camera of client machine
-        self.video = cv2.VideoCapture(0)
+        thread = threading.Thread(target=self.keep_processing, args=())
+        thread.daemon = True
+        thread.start()
 
-    def __del__(self):
-        self.video.release()
+    def process_one(self):
+        if not self.to_process:
+            return
+
+        # input is an ascii string. 
+        input_str = self.to_process.pop(0)
+
+        # convert it to a pil image
+        input_img = base64_to_pil_image(input_str)
+
+        ################## where the hard work is done ############
+        # output_img is an PIL image
+        output_img = self.makeup_artist.apply_makeup(input_img)
+
+        # output_str is a base64 string in ascii
+        output_str = pil_image_to_base64(output_img)
+
+        # convert eh base64 string in ascii to base64 string in _bytes_
+        self.to_output.append(binascii.a2b_base64(output_str))
+
+    def keep_processing(self):
+        while True:
+            self.process_one()
+            sleep(0.01)
+
+    def enqueue_input(self, input):
+        self.to_process.append(input)
 
     def get_frame(self):
-        success, image = self.video.read()
-        image = cv2.flip(image, 1)  # Flip image vertically
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Detect faces
-        faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.2, minNeighbors=5)
-
-        for x, y, w, h in faces:
-
-            # For each face identified draw rectangle around it
-            cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0), 3)
-
-            # Extract face alone to detect eyes
-            face_gray  = gray_image[y:y+h, x:x+w]
-
-            # Detect eyes
-            eyes = eye_cascade.detectMultiScale(face_gray, scaleFactor=1.1)
-
-            # Execute only if the number of eyes object identified is less than 3
-            if len(eyes) < 3:
-                for ex, ey, ew, eh in eyes:
-                    if (y+(h/2)) > (y+ey+(eh/2)):  # Eliminate nose being identified as eye
-
-                        # For each eye identified draw rectangle around it
-                        cv2.rectangle(image, (x+ex, y+ey), (x+ex+ew, y+ey+eh), (255,0,0), 2)
-
-        # JPEGs are being painted on the UI continuously as video stream.
-        # However OpenCV identifies the faces on raw images.
-        # So before sending response to UI the byte arrays is required to be encode into JPEG.
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+        while not self.to_output:
+            sleep(0.05)
+        return self.to_output.pop(0)
