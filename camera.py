306f326974f4e6149351b1e73edd2676d91040ac
asmaa-kashef <58347664+asmaa-kashef@users.noreply.github.com>
2021-12-25 12:58:34 +0200

    commit

--- camera.py
+++ camera.py
@@ -1,79 +1,48 @@
+# Import OpenCV library
 import cv2
-import threading
 
-class RecordingThread (threading.Thread):
-    def __init__(self, name, camera):
-        threading.Thread.__init__(self)
-        self.name = name
-        self.isRunning = True
-
-        self.cap = camera
-        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        self.out = cv2.VideoWriter('./static/video.avi',fourcc, 20.0, (640,480))
-
-    def run(self):
-        while self.isRunning:
-            ret, frame = self.cap.read()
-            if ret:
-                self.out.write(frame)
-
-        self.out.release()
-
-    def stop(self):
-        self.isRunning = False
-
-    def __del__(self):
-        self.out.release()
+# Create Face detection object from OpenCV frontal face Classifier
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade  = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 class VideoCamera(object):
+
     def __init__(self):
-        # Open a camera
-        self.cap = cv2.VideoCapture(0)
-      
-        # Initialize video recording environment
-        self.is_record = False
-        self.out = None
+        # Using OpenCV initiate front facing built-in camera of client machine
+        self.video = cv2.VideoCapture(0)
 
-        # Thread for recording
-        self.recordingThread = None
-    
     def __del__(self):
-        self.cap.release()
-    
+        self.video.release()
+
     def get_frame(self):
-        ret, frame = self.cap.read()
+        success, image = self.video.read()
+        image = cv2.flip(image, 1)  # Flip image vertically
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        if ret:
-            ret, jpeg = cv2.imencode('.jpg', frame)
+        # Detect faces
+        faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.2, minNeighbors=5)
 
-            # Record video
-            # if self.is_record:
-            #     if self.out == None:
-            #         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-            #         self.out = cv2.VideoWriter('./static/video.avi',fourcc, 20.0, (640,480))
-                
-            #     ret, frame = self.cap.read()
-            #     if ret:
-            #         self.out.write(frame)
-            # else:
-            #     if self.out != None:
-            #         self.out.release()
-            #         self.out = None  
+        for x, y, w, h in faces:
 
-            return jpeg.tobytes()
-      
-        else:
-            return None
+            # For each face identified draw rectangle around it
+            cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0), 3)
 
-    def start_record(self):
-        self.is_record = True
-        self.recordingThread = RecordingThread("Video Recording Thread", self.cap)
-        self.recordingThread.start()
+            # Extract face alone to detect eyes
+            face_gray  = gray_image[y:y+h, x:x+w]
 
-    def stop_record(self):
-        self.is_record = False
+            # Detect eyes
+            eyes = eye_cascade.detectMultiScale(face_gray, scaleFactor=1.1)
 
-        if self.recordingThread != None:
-            self.recordingThread.stop()
+            # Execute only if the number of eyes object identified is less than 3
+            if len(eyes) < 3:
+                for ex, ey, ew, eh in eyes:
+                    if (y+(h/2)) > (y+ey+(eh/2)):  # Eliminate nose being identified as eye
 
-            
+                        # For each eye identified draw rectangle around it
+                        cv2.rectangle(image, (x+ex, y+ey), (x+ex+ew, y+ey+eh), (255,0,0), 2)
+
+        # JPEGs are being painted on the UI continuously as video stream.
+        # However OpenCV identifies the faces on raw images.
+        # So before sending response to UI the byte arrays is required to be encode into JPEG.
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
