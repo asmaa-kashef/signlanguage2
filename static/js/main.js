f2d453f653cc18a02030a11a116dc1ae2d65c7cc
asmaa-kashef <58347664+asmaa-kashef@users.noreply.github.com>
2021-12-27 14:12:27 +0200

    commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,17 @@
-$(document).ready(function(){
-  let namespace = "/test";
+
   let video = document.querySelector("#videoElement");
   let canvas = document.querySelector("#canvasElement");
   let ctx = canvas.getContext('2d');
   photo = document.getElementById('photo');
+  let camera_button = document.querySelector("#start-camera");
+  let start_button = document.querySelector("#start-record");
+  let stop_button = document.querySelector("#stop-record");
+  let download_link = document.querySelector("#download-video");
   var localMediaStream = null;
+  let media_recorder = null;
+  let blobs_recorded = [];
+  let namespace = "/test";
+  var stream =null;
 
   var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
 
@@ -42,16 +49,43 @@ $(document).ready(function(){
       height: { min: 480 }
     }
   };
-
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+ 
+ 
+camera_button.addEventListener('click', async function() {
+ camera_stream=navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
     video.srcObject = stream;
     localMediaStream = stream;
-
     setInterval(function () {
       sendSnapshot();
     }, 50);
+   
   }).catch(function(error) {
     console.log(error);
   });
+ 
 });
 
+ start_button.addEventListener('click', function() {
+  alert(video.srcObject) 
+    // set MIME type of recording as video/webm
+    media_recorder = new MediaRecorder(video.srcObject, { mimeType: 'video/webm' });
+    
+    // event : new recorded video blob available 
+    media_recorder.addEventListener('dataavailable', function(e) {
+    blobs_recorded.push(e.data);
+    });
+
+    // event : recording stopped & all blobs sent
+  media_recorder.addEventListener('stop', function() {
+      // create local object URL from the recorded video blobs
+      let video_local = URL.createObjectURL(new Blob(blobs_recorded, { type: 'video/webm' }));
+      download_link.href = video_local;
+    });
+
+    // start recording with each recorded blob having 1 second video
+    media_recorder.start(1000);
+});
+
+stop_button.addEventListener('click', function() {
+  media_recorder.stop(); 
+});
